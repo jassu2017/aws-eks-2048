4b16ff4d2e87e49d04b0ac6a4577465c3c3e4fb0
Jayshree Prasad <jayshree.prasad@live.com>
2025-12-10 05:55:40 +0000

    added the codes

--- eks-demo/modules/vpc/input.tf
+++ eks-demo/modules/vpc/input.tf
@@ -8,3 +8,18 @@ variable "vpc_cidr" {
   description = "VPC CIDR block"
   type        = string
 }
+
+variable "public_subnet_cidr" {
+  description = "public subnet CIDR block"
+  type        = list(string)
+}
+
+variable "private_subnet_cidr" {
+  description = "public subnet CIDR block"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+}
